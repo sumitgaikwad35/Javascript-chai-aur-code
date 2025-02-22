3254fe35e09c31cdd71effe8a47d184dfdf9c2a1
sumitgaikwad35 <113461657+sumitgaikwad35@users.noreply.github.com>
2025-02-22 09:59:12 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,19 @@
+let names = "sumit"
+let roll = 35
+let city; // undefined 
+temp = null;
+
+console.log(typeof names);
+
+// number
+// bigint
+// string
+// null = standalone value
+// undefined 
+// Boolean
+// symbol = unique
+
+// object 
+
+console.log(typeof null);
+console.log(typeof city);
