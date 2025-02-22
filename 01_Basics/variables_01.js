53be5be6c613ff3a61865ad000518074b8f8add1
sumitgaikwad35 <113461657+sumitgaikwad35@users.noreply.github.com>
2025-02-22 05:37:13 +0000

    variables

---
+++ 01_Basics/variables_01.js
@@ -0,0 +1,10 @@
+const accountId = 21  // cannot change its value
+let emailId = "sumit@gmail.com"
+var names = "sumit"  // avoid using var because of issue in block scope and functional scope
+city = "Mumbai"
+let state; // undefined
+
+// console.log(emailId);
+console.table([accountId,emailId,names,city,state])
+
+
